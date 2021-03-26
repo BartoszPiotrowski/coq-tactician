b8d671be69c55538a4fdee7a8f1c894a9b826602
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-03-26 03:28:30 +0100

    do not remove old

--- src/forest_online_learner.ml
+++ src/forest_online_learner.ml
@@ -16,7 +16,7 @@ module OnlineForest : TacticianOnlineLearnerType = functor (TS : TacticianStruct
 
     let add forest b obj =
       let feats = proof_state_to_ints b in
-      Forest.add ~n_trees:320 ~remove_old:true
+      Forest.add ~n_trees:320 ~remove_old:false
       forest (Data.labeled (feats, obj))
 
     let learn db _loc outcomes tac =
